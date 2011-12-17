3af0045ccd5a77ec6450d2f9c171375451ebefda
Jon Ege Ronnenberg <jon.ronnenberg@gmail.com>
2011-12-17 03:28:51 +0100

    committing before pushing to github.com

--- wizard.source.js
+++ wizard.source.js
@@ -1,6 +1,6 @@
 var Wizard = function(config){
 	var	config = config || {},
-		_handlers = config.handlers || [],
+		_handlers = [],
 		_pages = config.pages || 0;
 
 /*=DEBUG */
@@ -14,22 +14,48 @@ if(typeof _pages == 'number') console.log(_pages); else console.dir(_pages);
 	}
 
 	this.addHandler = function(type, handler, options){
-		_handlers[type] = handler;
+	    _handlers.push([ util.getEnumName(this.Type, type), handler, options ]);
 /*=DEBUG */
-console.log(util.getEnumName(this.Type, type));
-
+console.log("Type", util.getEnumName(this.Type, type));
+console.dir(_handlers);
 	}
 	
 	this.fireEvent = function(type){
-        _handlers[type]();
+	    var self = this;
+	    /* sorted approach */
+	    _handlers.sort(function(a,b){ return a[0]-b[0]; });
+	    var start = _handlers.indexOf(util.getEnumName(self.Type, type));
+	    var end = _handlers.lastIndexOf(util.getEnumName(self.Type, type));
+	    /* filter approach */
+	    
+	    /* forEach approach */
+	    _handlers.forEach(function(value, index, array){
+	        if(util.getEnumName(self.Type, type) === value[0]){
+	            value[1].call(self, value[2]);
+	        }
+	    });
 /*=DEBUG */
 console.log(util.getEnumName(this.Type, type));
 	    
 	}
 	init();
 }
-Wizard.prototype.next = function(){ console.log('next'); }
-Wizard.prototype.prev = function(){ console.log('prev'); }
+Wizard.prototype.next = function(){
+/*=DEBUG */
+console.log('next');
+
+    this.fireEvent(this.Type.effectBefore);
+    this.fireEvent(this.Type.pageNext);
+    this.fireEvent(this.Type.effectAfter);
+}
+Wizard.prototype.prev = function(){
+/*=DEBUG */
+console.log('prev');
+
+    this.fireEvent(this.Type.effectBefore);
+    this.fireEvent(this.Type.pagePrev);
+    this.fireEvent(this.Type.effectAfter);
+}
 
 Wizard.prototype.addBeforeEffect = function(page, handler){
 	this.addHandler(this.Type.effectBefore, handler, { page: page });
@@ -39,5 +65,7 @@ Wizard.prototype.addAfterEffect = function(page, handler){
 }
 Wizard.prototype.Type = {
 	effectBefore: 1,
-	effectAfter: 2
+	effectAfter: 2,
+	pageNext: 4,
+	pagePrev: 8
 }
